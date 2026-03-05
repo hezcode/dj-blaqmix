1cc52f3cf72f3ad27bb3d6eafd13ba718751012c
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2026-03-05 16:19:01 +0300

    set CDN use for sanity to false inorder to allow fetching from sanity when updates are made for events

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -4,7 +4,7 @@ export const sanityConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "2025-01-01",
-  useCdn: true,
+  useCdn: false,
 };
 
 export const isSanityConfigured = Boolean(sanityConfig.projectId);
